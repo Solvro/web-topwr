ee7f2e10d50c9e9da72cbe94c30b974bef1d43b2
Dawid Kowal <dawidkowal04@gmail.com>
2025-04-18 21:08:26 +0200

    fix: eslint errors

---
+++ src/hooks/use-query-wrapper.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import type { QueryOptions } from "@tanstack/react-query";
+
+export function useQueryWrapper<TData, TError>(
+  queryKey: string,
+  queryFunction: Promise<TData>,
+  options?: Omit<QueryOptions<TData, TError>, "queryKey" | "queryFn">,
+) {
+  return useQuery<TData, TError>({
+    queryFn: async () => await queryFunction,
+    //@eslint-disable-next-line @tanstack/query/exhaustive-deps
+    queryKey: [queryKey, queryFunction],
+    ...options,
+  });
+}
