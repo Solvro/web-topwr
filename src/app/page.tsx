ed1a268c1452a730df598e6eee9274b329ef7d9c
michalges <michal.ges.mail@gmail.com>
2025-05-13 18:27:16 +0200

    feat: add home and login pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,57 @@
-import Image from "next/image";
+import { BookOpen, Building, RefreshCcw, Send } from "lucide-react";
+import type { LucideIcon } from "lucide-react";
 import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-10 overflow-hidden bg-gradient-to-br from-slate-950 to-blue-900">
-      <Image
-        className="px-4"
-        src="/logo.svg"
-        alt="Solvro Logo"
-        style={{ objectFit: "cover" }}
-        width={400}
-        height={400}
-        priority
-      />
-      <h1 className="px-4 text-center font-sans text-xl text-white md:text-4xl">
-        Twoja aplikacja NEXT.JS się odpala
-      </h1>
-      <div className="flex flex-row items-center justify-center gap-4 pt-10">
-        <Button asChild>
-          <Link href="https://docs.solvro.pl/">Docs Solvro</Link>
-        </Button>
-        <Button asChild>
-          <Link href="https://github.com/orgs/Solvro/teams/kn-solvro">
-            <Image
-              src="/github-mark.svg"
-              alt="Github"
-              style={{ objectFit: "cover" }}
-              width={20}
-              height={20}
-            />
-            Github
-          </Link>
-        </Button>
+    <div className="container mx-auto flex h-full items-center justify-center">
+      <div className="container mx-auto grid max-w-[1280px] grid-cols-1 gap-8 p-4 md:grid-cols-2">
+        <DashboardButton
+          href="/organizations"
+          icon={Building}
+          label="Zarządzanie organizacjami"
+        />
+        <DashboardButton
+          href="/"
+          icon={BookOpen}
+          label="Zarządzanie artykułami"
+        />
+        <DashboardButton
+          href="/"
+          icon={RefreshCcw}
+          label="Review zmian"
+          variant="outline"
+        />
+        <DashboardButton
+          href="/"
+          icon={Send}
+          label="Wyślij powiadomienie"
+          variant="outline"
+        />
       </div>
-      <footer className="absolute right-0 bottom-0 left-0 p-4 text-center text-white">
-        <p className="text-sm">
-          Made with ❤️ by{" "}
-          <Link href="https://solvro.pwr.edu.pl/pl/"> Solvro </Link>
-        </p>
-      </footer>
     </div>
+  );
+}
+
+function DashboardButton({
+  href,
+  icon: Icon,
+  label,
+  variant = "default",
+}: {
+  href: string;
+  icon: LucideIcon;
+  label: string;
+  variant?: "default" | "outline";
+}) {
+  return (
+    <Link href={href} passHref>
+      <Button className="h-20 w-full justify-start space-x-2" variant={variant}>
+        <Icon style={{ width: 20, height: 20 }} />
+        <span className="text-xl">{label}</span>
+      </Button>
+    </Link>
   );
 }
---
+++ src/schemas/index.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+export const LoginSchema = z.object({
+  email: z.string().email("Invalid email address"),
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters long")
+    .max(20, "Password must be at most 20 characters long"),
+});
