9ec9bc57a2861299acf53f00881aa487c69ccfe9
Konrad Guzek <konrad@guzek.uk>
2025-06-25 22:57:58 +0200

    fix: minor issues™

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -7,7 +7,7 @@ import { globalStore } from "./global";
 export const authStateAtom = atom<AuthState | null>(null);
 
 /**
- * An atom accesser for use **outside** of React components.
+ * An atom accessor for use **outside** of React components.
  * @see {@link @/hooks/use-auth.useAuth} for the React hook version.
  */
 export const getAuthState = () => globalStore.get(authStateAtom);
