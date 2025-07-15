038e527bedacafcce7f562582328d3db76d809e6
Dawid Kowal <dawidkowal04@gmail.com>
2025-07-15 16:23:06 +0200

    feat: add core calendar and add event form functionality without submit logic

--- src/components/calendar/day-button.tsx
+++ src/components/calendar/day-button.tsx
@@ -1,20 +1,49 @@
+import type { DateObject } from "@/lib/types";
+import { cn } from "@/lib/utils";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
+import { AddEventForm } from "./add-event-form";
+
 interface Props {
   day: number;
-  onClick: (day: number) => void;
+  today: DateObject;
+  clickable?: boolean;
 }
 
-export function DayButton({ day, onClick }: Props) {
+export function DayButton({ day, today, clickable = false }: Props) {
   const isCurrentDay = day === new Date().getDate();
+
   return (
-    <button
-      className={`flex h-32 cursor-pointer items-center justify-center border ${
-        isCurrentDay ? "bg-blue-500 text-white" : "bg-white text-black"
-      }`}
-      onClick={() => {
-        onClick(day);
-      }}
-    >
-      {day}
-    </button>
+    <Dialog>
+      <DialogTrigger>
+        <div
+          className={cn(
+            "flex h-20 items-center justify-center border",
+            isCurrentDay ? "bg-blue-500 text-white" : "bg-white text-black",
+            clickable ? "cursor-pointer hover:bg-gray-100" : "cursor-default",
+          )}
+        >
+          <button disabled={!clickable}>{day}</button>
+        </div>
+      </DialogTrigger>
+      <DialogContent className="max-h-[80vh] w-max">
+        <DialogHeader>
+          <DialogTitle>
+            {today.day} {today.month.name} {today.year}
+          </DialogTitle>
+          <DialogDescription></DialogDescription>
+        </DialogHeader>
+        <div className="mx-auto w-full">
+          <AddEventForm />
+        </div>
+      </DialogContent>
+    </Dialog>
   );
 }
