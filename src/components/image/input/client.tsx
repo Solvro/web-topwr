60f2e4bfeabfaec61bb654628db5946b40615d8d
Konrad Guzek <konrad@guzek.uk>
2025-07-25 06:00:45 +0200

    fix: optimize input image fetching

--- src/components/image/input/client.tsx
+++ src/components/image/input/client.tsx
@@ -1,47 +1,23 @@
 "use client";
 
-import { Camera, Loader } from "lucide-react";
-import Image from "next/image";
-import { useEffect, useState } from "react";
-import type { ControllerRenderProps, Path } from "react-hook-form";
+import { Camera } from "lucide-react";
+import type { ReactNode } from "react";
 import { toast } from "sonner";
-import type { z } from "zod";
 
 import { FormControl, FormLabel } from "@/components/ui/form";
 import { useMutationWrapper } from "@/hooks/use-mutation-wrapper";
-import { useQueryWrapper } from "@/hooks/use-query-wrapper";
-import { fetchMutation, fetchQuery } from "@/lib/fetch-utils";
-import type { FileEntry } from "@/types/api";
+import { fetchMutation } from "@/lib/fetch-utils";
 
-export function ImageInput<T extends z.infer<z.ZodObject<z.ZodRawShape>>>({
-  field,
+import type { ImageInputProps, ImageInputPropsGeneric } from ".";
+
+export function ImageInputClient<T extends ImageInputPropsGeneric>({
+  name,
+  onChange,
   label,
-}: {
-  field: ControllerRenderProps<T, Path<T>>;
-  label: string;
-}) {
-  const [fileUrl, setFileUrl] = useState("");
-
-  const metadataQuery = useQueryWrapper(
-    `read__files__image__${field.name}`,
-    async () => {
-      if (
-        field.value == null ||
-        field.value === "" ||
-        typeof field.value !== "string"
-      ) {
-        return null;
-      }
-      const response = await fetchQuery<FileEntry>(
-        `/files/${field.value as string}`,
-      );
-      setFileUrl(response.url);
-      return response;
-    },
-  );
-
+  existingImage,
+}: ImageInputProps<T> & { existingImage: ReactNode }) {
   const uploadMutation = useMutationWrapper(
-    `create__files__image__${field.name}`,
+    `create__files__image__${name}`,
     async (file: File) => {
       const formData = new FormData();
       formData.append("file", file);
@@ -51,14 +27,10 @@ export function ImageInput<T extends z.infer<z.ZodObject<z.ZodRawShape>>>({
         body: formData,
       });
       const [uuid, _fileExtension] = response.key.split(".");
-      field.onChange(uuid);
+      onChange(uuid);
       return response;
     },
   );
-
-  useEffect(() => {
-    void metadataQuery.refetch();
-  }, [metadataQuery]);
 
   return (
     <FormLabel className="flex flex-col items-start space-y-1.5">
@@ -82,23 +54,13 @@ export function ImageInput<T extends z.infer<z.ZodObject<z.ZodRawShape>>>({
         />
       </FormControl>
       <div className="bg-background border-input flex aspect-video w-full cursor-pointer flex-col items-center justify-center overflow-hidden rounded-lg border md:h-48 md:w-48">
-        {field.value === "" || field.value == null ? (
+        {existingImage ?? (
           <>
             <Camera className="text-image-input-icon h-12 w-12" />
             <span className="text-muted-foreground text-xs">
               Kliknij, aby dodać zdjęcie
             </span>
           </>
-        ) : fileUrl === "" ? (
-          <Loader className="animate-spin" />
-        ) : (
-          <Image
-            src={fileUrl}
-            alt={label}
-            className="h-full max-w-full object-cover"
-            width={256}
-            height={256}
-          />
         )}
       </div>
     </FormLabel>
