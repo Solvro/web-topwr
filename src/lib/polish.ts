71e658c6ea512482a199ab68a563a215b04fa3af
Konrad Guzek <konrad@guzek.uk>
2025-09-10 00:44:48 +0200

    refactor: noun declensions, toast messages and form inputs

--- src/lib/polish.ts
+++ src/lib/polish.ts
@@ -1,35 +1,31 @@
-import {
-  DETERMINER_DECLENSIONS,
-  RESOURCE_DECLENSIONS,
-} from "@/config/constants";
-import type { DeclensionCase, Resource } from "@/config/enums";
-import type { Declensions } from "@/types/app";
+import { DETERMINER_DECLENSIONS, NOUN_DECLENSIONS } from "@/config/constants";
+import type { DeclensionCase, GrammaticalGender } from "@/config/enums";
+import type { Declensions, DeclinableNoun } from "@/types/app";
 
 interface DeclensionOptions {
-  case?: DeclensionCase;
   prependDeterminer?: boolean;
   plural?: boolean;
 }
 
 /**
- * Returns the declined form of a resource noun based on the specified case.
- * Optionally prepends the determiner based on the resource's gender.
- * @param resource - The resource to decline.
+ * Returns the declined form of a noun based on the specified case.
+ * Optionally prepends the determiner based on the noun's gender.
+ * @param noun - The noun to decline.
  * @param declensionOptions - Additional options for declension.
  * @param declensionOptions.prependDeterminer - Whether to prepend the determiner (i.e. to, ten, ta in the correct declension).
  * @param declensionOptions.plural - Whether to use the plural form.
- * @returns All declensions of the resource noun.
+ * @returns All declensions of the noun.
  * @example
  * const declensions: Declensions = declineNoun(Resource.GuideArticles);
  */
 export function declineNoun(
-  resource: Resource,
-  declensionOptions?: Omit<DeclensionOptions, "case">,
-): Declensions;
+  noun: DeclinableNoun,
+  declensionOptions?: DeclensionOptions,
+): Declensions & { gender: GrammaticalGender };
 /**
- * Returns the declined form of a resource noun based on the specified case.
- * Optionally prepends the determiner based on the resource's gender.
- * @param resource - The resource to decline.
+ * Returns the declined form of a noun based on the specified case.
+ * Optionally prepends the determiner based on the noun's gender.
+ * @param noun - The noun to decline.
  * @param declensionOptions - Additional options for declension.
  * @param declensionOptions.case - The grammatical case to use for declension. If not provided, returns all declensions.
  * @param declensionOptions.prependDeterminer - Whether to prepend the determiner (i.e. to, ten, ta in the correct declension).
@@ -40,25 +36,25 @@ export function declineNoun(
  * declineNoun(Resource.StudentOrganizations, { case: DeclensionCase.Accusative, plural: true }) === 'organizacje studenckie'
  */
 export function declineNoun(
-  resource: Resource,
-  declensionOptions: { case: DeclensionCase } & Omit<DeclensionOptions, "case">,
+  noun: DeclinableNoun,
+  declensionOptions: DeclensionOptions & { case: DeclensionCase },
 ): string;
 
 export function declineNoun(
-  resource: Resource,
+  noun: DeclinableNoun,
   {
     case: declensionCase,
     prependDeterminer = false,
     plural = false,
-  }: DeclensionOptions = {},
-): string | Declensions {
+  }: DeclensionOptions & { case?: DeclensionCase } = {},
+): string | (Declensions & { gender: GrammaticalGender }) {
   const quantity = plural ? "plural" : "singular";
-  const declensions = RESOURCE_DECLENSIONS[quantity][resource];
+  const { gender, [quantity]: declensions } = NOUN_DECLENSIONS[noun];
   if (declensionCase == null) {
-    return declensions;
+    return { ...declensions, gender };
   }
   const result = declensions[declensionCase];
   return prependDeterminer
-    ? `${DETERMINER_DECLENSIONS[quantity][declensions.gender][declensionCase]} ${result}`
+    ? `${DETERMINER_DECLENSIONS[gender][quantity][declensionCase]} ${result}`
     : result;
 }
