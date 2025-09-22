6ebd19e2ccba2c6003e92480086664f16e0210d5
Dawid Kowal <dawidkowal04@gmail.com>
2025-09-22 15:53:40 +0200

    chore: remove residual backup path

--- src/app/event_calendar_old/page.tsx
+++
@@ -1,9 +0,0 @@
-import { CalendarOld } from "@/components/calendar_old.backup/calendar";
-
-export default function CalendarPage() {
-  return (
-    <div>
-      <CalendarOld clickable={true} />
-    </div>
-  );
-}
