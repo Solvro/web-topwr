eb24333c9e47c6e6d1620ff6b8cba986751c561c
michalges <michal.ges.mail@gmail.com>
2025-10-23 18:13:25 +0200

    fix: remove old styles

--- src/components/inputs/input-slot.tsx
+++ src/components/inputs/input-slot.tsx
@@ -1,4 +1,4 @@
-import { Slot } from "radix-ui";
+import { Slot } from "@radix-ui/react-slot";
 
 import { cn } from "@/lib/utils";
 
@@ -7,13 +7,13 @@ export function InputSlot({
   asChild,
   ...props
 }: { asChild?: boolean } & React.HTMLAttributes<HTMLElement>) {
-  const Comp = (asChild ?? false) ? Slot.Root : "input";
+  const Comp = (asChild ?? false) ? Slot : "input";
   return (
     <Comp
       className={cn(
         "bg-background hover:bg-background/20",
-        "border-input aria-invalid:border-destructive border",
-        "focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 focus-visible:ring-[3px]",
+        "border-input aria-invalid:border-destructive focus-visible:border-ring border",
+        "focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 focus-visible:ring-[3px]",
         "shadow-xs",
       )}
       {...props}
