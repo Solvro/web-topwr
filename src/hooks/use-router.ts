37784a70e740ba1c23104a81a6cc2e1d3383c17a
Konrad Guzek <konrad@guzek.uk>
2025-11-09 12:24:10 +0100

    docs(hooks): useRouter docstring grammar

--- src/hooks/use-router.ts
+++ src/hooks/use-router.ts
@@ -5,8 +5,8 @@ type TransitionRouter = ReturnType<typeof useTransitionRouter>;
 type NavigateOptionsWithTransition = Parameters<TransitionRouter["push"]>[1];
 
 /**
- * This hook types `useRouter` from `useTransitionRouter` from `@solvro/next-view-transitions`
- * using typed routes, ensuring type safety when navigating between pages with transitions.
+ * This hook wraps `useTransitionRouter` from `@solvro/next-view-transitions`,
+ * using typed routes to ensure type safety when navigating between pages with transitions.
  */
 export const useRouter = () => {
   const transitionRouter = useTransitionRouter();
