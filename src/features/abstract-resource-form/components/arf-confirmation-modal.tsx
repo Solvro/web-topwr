72ba442045403c9e6e9721e78bb1f0f622cea6ed
Konrad Guzek <konrad@guzek.uk>
2025-11-14 21:48:53 +0100

    fix(arf): remove redundant setState

--- src/features/abstract-resource-form/components/arf-confirmation-modal.tsx
+++ src/features/abstract-resource-form/components/arf-confirmation-modal.tsx
@@ -67,7 +67,6 @@ export function ArfConfirmationModal<T extends Resource>({
             <Button
               onClick={async () => {
                 await onSubmit();
-                setIsModalOpen(false);
               }}
               {...props}
             />
