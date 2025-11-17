bc59236ea4b39d9ebbab0dd0ab6af5c4e9c2b06b
michalges <michal.ges.mail@gmail.com>
2025-11-17 21:03:02 +0100

    refactor: split get-badge-labels into mulitple functions

--- src/features/abstract-resource-collection/components/arl-item-drag-handle.tsx
+++ src/features/abstract-resource-collection/components/arl-item-drag-handle.tsx
@@ -3,11 +3,16 @@
 import { useSortable } from "@dnd-kit/sortable";
 import { GripHorizontal } from "lucide-react";
 
+import type { Resource } from "@/features/resources";
 import { cn } from "@/lib/utils";
 
 import type { ListItem } from "../types/internal";
 
-export function ArlItemDragHandle({ item }: { item: ListItem }) {
+export function ArlItemDragHandle<T extends Resource>({
+  item,
+}: {
+  item: ListItem<T>;
+}) {
   const { attributes, listeners, isDragging } = useSortable({
     id: item.id,
   });
