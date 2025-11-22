58cf7b3a7cfd6f17614cc0f76568df64b909e517
Dawid Kowal <dawidkowal04@gmail.com>
2025-11-22 13:37:31 +0100

    feat(calendar): calendar cards

--- src/features/abstract-resource-calendar/components/arc-holiday-card.tsx
+++ src/features/abstract-resource-calendar/components/arc-holiday-card.tsx
@@ -1,15 +1,14 @@
 "use client";
 
-import { SquarePen } from "lucide-react";
-
-import { Button } from "@/components/ui/button";
 import { useArfSheet } from "@/features/abstract-resource-form";
-import { DeleteButtonWithDialog, Resource } from "@/features/resources";
+import { Resource } from "@/features/resources";
 import type {
   ResourceDataType,
   ResourceDefaultValues,
 } from "@/features/resources/types";
 import type { ResourceFormProps } from "@/types/components";
+
+import { SheetCard } from "./arc-sheet-card";
 
 export function HolidayCard({
   event,
@@ -20,7 +19,7 @@ export function HolidayCard({
   clickable: boolean;
   parentResourceData: ResourceDataType<Resource.AcademicSemesters>;
 }) {
-  const arfSheet = useArfSheet(Resource.AcademicSemesters);
+  const arfSheet = useArfSheet(Resource.AcademicSemesters as Resource);
 
   const formProps = {
     resource: Resource.Holidays,
@@ -28,37 +27,20 @@ export function HolidayCard({
     ...event,
   } satisfies ResourceFormProps<Resource.Holidays> &
     ResourceDefaultValues<Resource.Holidays>;
+
   return (
-    <article className="bg-accent flex w-full justify-between rounded-md p-3 text-left text-sm">
+    <SheetCard
+      event={event}
+      clickable={clickable}
+      parentResourceData={parentResourceData}
+      sheet={arfSheet}
+      formProps={formProps}
+      iconOnly
+    >
       <div className="my-auto">
         <div className="font-medium">{event.description}</div>
         <div className="mt-1 text-xs">{event.startDate}</div>
       </div>
-      {clickable ? (
-        <div className="flex items-center gap-2">
-          <Button
-            variant="ghost"
-            className="h-10 w-10"
-            onClick={() => {
-              arfSheet.showSheet(
-                {
-                  item: null,
-                  childResource: Resource.Holidays,
-                  parentResourceData,
-                },
-                formProps,
-              );
-            }}
-          >
-            <SquarePen />
-          </Button>
-          <DeleteButtonWithDialog
-            resource={Resource.Holidays}
-            itemName={event.description}
-            id={event.id}
-          />
-        </div>
-      ) : null}
-    </article>
+    </SheetCard>
   );
 }
