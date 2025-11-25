46d2fe02fa48d6a6efec0adbeb10a4b5aecfc54f
Jan Marek <janwieslaw.marek@gmail.com>
2025-11-25 23:40:07 +0100

    fix(rte): remove inconsistent skeletons for absent text

--- src/components/inputs/rich-text-skeleton.tsx
+++ src/components/inputs/rich-text-skeleton.tsx
@@ -147,9 +147,7 @@ export function RichTextSkeleton({
           />
         ) : (
           <div className="space-y-2">
-            <Skeleton className="h-4 w-3/4" />
-            <Skeleton className="h-4 w-full" />
-            <Skeleton className="h-4 w-5/6" />
+            <Skeleton className="h-6 w-3/4" />
           </div>
         )}
       </div>
