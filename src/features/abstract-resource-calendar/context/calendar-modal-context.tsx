a6077019ef5fc532ef6b77ada03b4afb988c2f0c
GTR1701 <dawidkowal04@gmail.com>
2025-11-28 16:30:22 +0100

    feat(calendar): useCalendarModal hook

--- src/features/abstract-resource-calendar/context/calendar-modal-context.tsx
+++ src/features/abstract-resource-calendar/context/calendar-modal-context.tsx
@@ -4,8 +4,5 @@ import { createContext } from "react";
 
 import type { CalendarModalContextValue } from "../types/internal";
 
-export const CalendarModalContext = createContext<CalendarModalContextValue>({
-  openSemesters: () => {
-    /* empty */
-  },
-});
+export const CalendarModalContext =
+  createContext<CalendarModalContextValue | null>(null);
