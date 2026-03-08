8feb55b4f82222a321f903e6e5d2c3243b55c95a
michalges <michal.ges.mail@gmail.com>
2026-03-09 00:08:13 +0100

    fix: redundant cast

--- src/components/inputs/array-input.tsx
+++ src/components/inputs/array-input.tsx
@@ -6,10 +6,7 @@ import { MultiSelect } from "@/components/ui/multi-select";
 import type { ArrayInputOptions } from "@/features/abstract-resource-form/types";
 import type { Resource } from "@/features/resources";
 import { getResourceMetadata, getResourcePkValue } from "@/features/resources";
-import type {
-  ArrayResources,
-  UnorderableResourceDataType,
-} from "@/features/resources/types";
+import type { ArrayResources } from "@/features/resources/types";
 import type { ResourceRelations } from "@/types/components";
 
 export function ArrayInput<T extends Resource>({
@@ -39,11 +36,7 @@ export function ArrayInput<T extends Resource>({
     if (
       value.includes(name) ||
       inputOptions.itemFilter == null ||
-      (
-        inputOptions.itemFilter as (
-          item: UnorderableResourceDataType<Resource>,
-        ) => boolean
-      )(item)
+      inputOptions.itemFilter(item)
     ) {
       accumulator.push(name);
     }
