cdb0bd7bea69ad0eca3800fc41a22fda84252ff7
GTR1701 <dawidkowal04@gmail.com>
2026-03-25 21:21:25 +0100

    chore: make guide article description optional

--- src/features/resources/schemas/guide-article-schema.ts
+++ src/features/resources/schemas/guide-article-schema.ts
@@ -6,8 +6,5 @@ export const GuideArticleSchema = z.object({
   title: RequiredStringSchema,
   imageKey: RequiredStringSchema,
   shortDesc: RequiredStringSchema,
-  // TODO: change when backend allows omitting the description
-  // https://github.com/Solvro/backend-topwr/issues/281
-  description: RequiredStringSchema,
-  // description: z.string().trim().nullish(),
+  description: z.string().trim().nullish(),
 });
