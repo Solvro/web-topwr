e2a57f8ceb340b0340ebd56989e344fb0ce62c6e
michalges <michal.ges.mail@gmail.com>
2026-03-27 10:20:01 +0100

    fix: missing default values

--- src/features/resources/data/resource-metadata.ts
+++ src/features/resources/data/resource-metadata.ts
@@ -1370,8 +1370,8 @@ export const RESOURCE_METADATA = {
       },
       defaultValues: {
         weekDay: Weekday.Monday,
-        openTime: "00:00:00",
-        closeTime: "00:00:00",
+        openTime: "08:00:00",
+        closeTime: "16:00:00",
         libraryId: -1,
       },
     },
@@ -1401,8 +1401,8 @@ export const RESOURCE_METADATA = {
       },
       defaultValues: {
         specialDate: "",
-        openTime: "08:00",
-        closeTime: "16:00",
+        openTime: "08:00:00",
+        closeTime: "16:00:00",
         libraryId: -1,
       },
     },
