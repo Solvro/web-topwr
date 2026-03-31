225085abba9b69aff4893ac095ddd85bd305d36f
gtr1701 <dawidkowal04@gmail.com>
2026-03-31 19:21:17 +0200

    feat(resources): das timetable schema

--- src/features/resources/data/resource-schemas.ts
+++ src/features/resources/data/resource-schemas.ts
@@ -18,6 +18,7 @@ import { DasLinkSchema } from "../schemas/das-link-schema";
 import { DasMapSchema } from "../schemas/das-map-schema";
 import { DasSchema } from "../schemas/das-schema";
 import { DasStandSchema } from "../schemas/das-stand-schema";
+import { DasTimetableSchema } from "../schemas/das-timetable-schema";
 import { DashboardSchema } from "../schemas/dashboard-schema";
 import { DaySwapSchema } from "../schemas/day-swap-schema";
 import { DepartmentLinkSchema } from "../schemas/department-link-schema";
@@ -65,6 +66,7 @@ export const RESOURCE_SCHEMAS = {
   [Resource.DasLink]: DasLinkSchema,
   [Resource.DasMap]: DasMapSchema,
   [Resource.DasStand]: DasStandSchema,
+  [Resource.DasTimetable]: DasTimetableSchema,
   [Resource.DaySwaps]: DaySwapSchema,
   [Resource.Departments]: DepartmentSchema,
   [Resource.DepartmentLinks]: DepartmentLinkSchema,
