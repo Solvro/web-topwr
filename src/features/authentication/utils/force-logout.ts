1481af18bd5e1585bebf0feb66014a50ed8effaf
gtr1701 <dawidkowal04@gmail.com>
2026-05-06 20:56:05 +0200

    chore(auth): soft-patch toast message circular import

--- src/features/authentication/utils/force-logout.ts
+++ src/features/authentication/utils/force-logout.ts
@@ -1,7 +1,8 @@
 import Cookies from "js-cookie";
 
 import { deferClientToast } from "@/features/toaster";
-import { getToastMessages } from "@/lib/get-toast-messages";
+
+// import { getToastMessages } from "@/lib/get-toast-messages";
 
 import { AUTH_STATE_COOKIE_NAME } from "../constants";
 
@@ -13,7 +14,8 @@ export function forceLogout(): void {
   Cookies.remove(AUTH_STATE_COOKIE_NAME);
   deferClientToast({
     level: "warning",
-    message: getToastMessages.auth.sessionExpired,
+    // TODO: Resolve the circular dependency issue to get the message from getToastMessages.auth.sessionExpired,
+    message: "Twoja sesja wygasła. Proszę zalogować się ponownie.",
   });
   window.location.href = "/login";
 }
