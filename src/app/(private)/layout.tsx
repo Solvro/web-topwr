5b2f6ef869699e261d6e15b6b6b7352b7b4acf64
michalges <michal.ges.mail@gmail.com>
2026-05-10 22:18:13 +0200

    chore: update metadata

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -1,1 +1,7 @@
+import type { Metadata } from "next";
+
 export { PrivateLayout as default } from "@/features/authentication/server";
+
+export const metadata: Metadata = {
+  title: "ToPWR | Panel Administratora",
+};
--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -1,1 +1,7 @@
+import type { Metadata } from "next";
+
 export { LoginPage as default } from "@/features/authentication";
+
+export const metadata: Metadata = {
+  title: "ToPWR | Panel Administratora - Logowanie",
+};
