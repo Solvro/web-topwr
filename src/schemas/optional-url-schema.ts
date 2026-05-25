b3badc8c545faf21751fa1a0fcf6c545f1f48566
jakub206 <jakub_dluzak@wp.pl>
2026-05-25 17:31:23 +0200

    fix: better url validation

--- src/schemas/optional-url-schema.ts
+++ src/schemas/optional-url-schema.ts
@@ -6,8 +6,8 @@ export const OptionalUrlSchema = z
   .string()
   .trim()
   .url(FORM_ERROR_MESSAGES.INVALID_URL)
-  .startsWith("http://", {
-    message: FORM_ERROR_MESSAGES.INVALID_URL,
+  .startsWith("https://", {
+    message: FORM_ERROR_MESSAGES.INVALID_URL_SCHEME,
   })
   .or(z.literal(""))
   .nullish();
