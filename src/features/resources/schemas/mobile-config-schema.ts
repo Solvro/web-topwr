79b370880d6bbb58290bcaa79af2ac8554a45f2b
michalges <michal.ges.mail@gmail.com>
2026-07-06 16:37:03 +0200

    feat: add booths to mobile config

--- src/features/resources/schemas/mobile-config-schema.ts
+++ src/features/resources/schemas/mobile-config-schema.ts
@@ -3,6 +3,8 @@ import { z } from "zod";
 import { PositiveIntegerSchema, RequiredUrlSchema } from "@/schemas";
 
 export const MobileConfigSchema = z.object({
+  boothsApiBaseUrl: RequiredUrlSchema,
+  boothsEnabled: z.boolean(),
   sksMicroserviceUrl: RequiredUrlSchema,
   parkingMicroserviceUrl: RequiredUrlSchema,
   cmsReferenceNumber: PositiveIntegerSchema,
